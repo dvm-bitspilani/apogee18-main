d92479f0e1ece7038e839d72a40bae551099226f
Roboneet <neethumariyajoy@gmail.com>
2018-01-19 13:20:14 +0530

    links

--- js/animations.js
+++ js/animations.js
@@ -1,7 +1,7 @@
 $(function init(){
 	if(window.innerWidth>768){
 		var links = document.querySelectorAll('.link');
-		console.log(links);
+		// console.log(links);
 		Array.prototype.forEach.call(links,letterAnimation)
 	}
 });
@@ -10,16 +10,17 @@ function letterAnimation(el, pre,{trigger=null}={}){
 	var name = el.innerText;
 	var spans = "";
 	var step = (.25)/(name.length);
+	// var step = 0.05;
 	// console.log(step, spans.length);
 	name.split('').forEach((letter, index)=>{
-		
+		// console.log(letter, index)
 		spans += ("<span class='letter "+ pre + index+ "' style='transition-delay: " + step*(index+1) + "s'>" + letter + "</span>");
 	})
 
 	var string = "<span class='top'>" + spans + "</span><span class='base'>" + spans + "</span>";
 	el.innerHTML = string;
 	var trigger = trigger || el;
-	console.log(trigger)
+	// console.log(trigger)
 	trigger.addEventListener('mouseenter', ()=>{
 		trigger.className += " hover_animation";
 		setTimeout(()=>{
