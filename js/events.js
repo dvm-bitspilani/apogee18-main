fe265cd985e87366c31ff7a54785f98940b5a59a
Chinmay Pandhare <pandharechinmay882@gmail.com>
2018-01-19 08:27:34 +0530

    Update contacts

--- js/events.js
+++ js/events.js
@@ -15,3 +15,15 @@ $(document.body).on("click","#eventCat li",function(e){
         }
     }
 });
+$("#nextEvent").click(function(){
+    if(parseInt($($(".event")[0]).css('margin-left')) >= - $('.event').length*440 + 880) {
+        $($(".event")[0]).css('margin-left',"-=440px");
+        $($(".desc")[0]).css('margin-left',"-=440px");
+    }
+});
+$("#prevEvent").click(function(){
+    if(parseInt($($(".event")[0]).css('margin-left')) < 20) {
+        $($(".event")[0]).css('margin-left',"+=440px");
+        $($(".desc")[0]).css('margin-left',"+=440px");
+    }
+});
