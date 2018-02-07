b2c67032d8e354a13d3dc1b773bfe28ce3f8667c
Chinmay Pandhare <pandharechinmay882@gmail.com>
2018-02-07 19:02:28 +0530

    Ham

---
+++ js/ham.js
@@ -0,0 +1,7 @@
+function showMenu() {
+    document.getElementById('hammenu').style.transform = "translateX(0)";
+}
+
+function hideMenu() {
+    document.getElementById('hammenu').style.transform = "translateX(-100%)";
+}
